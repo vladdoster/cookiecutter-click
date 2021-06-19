fa633c71eacc120e3f64f9a01d1fd70354106ded
Vladislav Doster <mvdoster@gmail.com>
2021-06-19 05:33:35 -0500

    (clean up)
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,26 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 
 TERMINATOR = "\x1b[0m"
-WARNING = "\x1b[1;33m [WARNING]: "
-INFO = "\x1b[1;33m [INFO]: "
-HINT = "\x1b[3;33m"
 SUCCESS = "\x1b[1;32m [SUCCESS]: "
 
-def remove_pylintrc_file():
-    os.remove(".pylintrc")
-    
-def remove_flake8_file():
-    os.remove(".flake8")
+def remove_file(f_name):
+    os.remove(f_name)
 
 def main():
     linter = "{{ cookiecutter.linter }}".lower()
     if linter == "flake8":
-        remove_pylintrc_file()
+        remove_file(".pylintrc")
     if linter == "pylint":
-        remove_flake8_file()
+        remove_file(".flake8")
 
-    print(SUCCESS + "Project initialized, keep up the good work!" + TERMINATOR)
+    print(f"{SUCCESS} Project initialized, keep up the good work!{TERMINATOR}")
 
 
 if __name__ == "__main__":
